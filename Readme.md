cc01b01ed41f23b30f6a4b1c98af45c0174598fc
Deepak Puthraya <deepakputhraya@users.noreply.github.com>
2019-10-11 18:44:08 +0530

    Implement action, add README (#1)

--- Readme.md
+++ Readme.md
@@ -1,2 +1,22 @@
 # Action Branch Name
+![](https://github.com/deepakputhraya/action-branch-name/workflows/.github/workflows/main.yml/badge.svg)
+
 Github action to enforce naming convention on branch names
+
+## Usage
+
+See [action.yml](./action.yml)
+
+```yaml
+steps:
+- uses: deepakputhraya/action-branch-name@master
+  with:
+    regex: '([a-z])+\/([a-z])+' # Regex the branch should match. This example enforces grouping
+    allowed_prefixes: 'feature,stable,fix' # All branches should start with the given prefix
+    ignore: master,develop # Ignore exactly matching branch names from convention
+    min_length: 5 # Min length of the branch name
+    max_length: 20 # Max length of the branch name
+```
+
+## License
+The scripts and documentation in this project are released under the [MIT License](./LICENSE)
--- index.js
+++ index.js
@@ -1,20 +1,71 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function validate() {
-    // Check if event is valid
+const validEvent = ['push', 'pull_request'];
+
+function getBranchName(eventName, payload) {
+    let branchName;
+    switch (eventName) {
+        case 'push':
+            payload.ref.replace('refs/heads/', '');
+            branchName = payload;
+            break;
+        case 'pull_request':
+            branchName = payload.pull_request.head.ref;
+            break;
+        default:
+            throw new Error(`Invalid event name: ${eventName}`);
+    }
+    return branchName;
 }
 
 async function run() {
     try {
-        const regex = core.getInput('regex');
-        console.log(`Regex: ${regex}!`);
-        const allowedPrefixes = core.getInput('allowed_prefixes');
-        console.log(`Regex: ${allowedPrefixes}`);
+        const eventName = github.context.eventName;
+        core.info(`Event name: ${eventName}`);
+        if (validEvent.indexOf(eventName) < 0) {
+            core.setFailed(`Invalid event: ${eventName}`);
+            return;
+        }
 
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        console.log(`The event payload: ${payload}`);
+        const branch = getBranchName(eventName, github.context.payload);
+        core.info(`Branch name: ${branch}`);
+        // Check if branch is to be ignored
+        const ignore = core.getInput('ignore');
+        if (ignore.length > 0 && ignore.split(',').some((el) => branch === el)) {
+            core.info(`Skipping checks since ${branch} is in the ignored list - ${ignore}`);
+            return
+        }
+
+        // Check if branch pass regex
+        const regex = RegExp(core.getInput('regex'));
+        core.info(`Regex: ${regex}`);
+        if (!regex.test(branch)) {
+            core.setFailed(`Branch ${branch} failed to pass match regex - ${regex}`);
+            return
+        }
+
+        // Check if branch starts with a prefix
+        const prefixes = core.getInput('allowed_prefixes');
+        core.info(`Allowed Prefixes: ${prefixes}`);
+        if (prefixes.length > 0 && !prefixes.split(',').some((el) => branch.startsWith(el))) {
+            core.setFailed(`Branch ${branch} failed did not match any of the prefixes - ${prefixes}`);
+            return
+        }
+
+        // Check min length
+        const minLen = parseInt(core.getInput('min_length'));
+        if (branch.length < minLen) {
+            core.setFailed(`Branch ${branch} is smaller than min length specified - ${minLen}`);
+            return
+        }
+
+        // Check max length
+        const maxLen = parseInt(core.getInput('max_length'));
+        if (maxLen > 0 && branch.length > maxLen) {
+            core.setFailed(`Branch ${branch} is greater than max length specified - ${maxLen}`);
+            return
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
